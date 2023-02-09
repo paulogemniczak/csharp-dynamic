eb3489f3f07f47068da6abdda5621bdc4d5e2a7b
Paulo Gemnizak <paulogemniczak@gmail.com>
2023-02-08 21:43:33 -0300

    Adicionar arquivos de projeto.

---
+++ csharp-dynamic/Program.cs
@@ -0,0 +1,11 @@
+﻿dynamic name = "Paulo";
+Console.WriteLine(name);
+name = 40;
+Console.WriteLine(name);
+
+name = "Daniele";
+// name++; //Microsoft.CSharp.RuntimeBinder.RuntimeBinderException: Operator '++' cannot be applied to operand of type 'string'
+
+dynamic a = 10; // dynamic {int}
+dynamic b = 5; // dynamic {int}
+var c = a + b; // dynamic {int}
